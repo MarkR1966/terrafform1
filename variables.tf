99e0799d9524a0c05810e6f6bf333b8cfed8bd93
MarkR1966 <markrafferty27@gmail.com>
2020-08-19 13:08:43 +0000

    VPC Changes

--- variables.tf
+++ variables.tf
@@ -22,3 +22,13 @@ variable "shared_key" {
   description = "This variable states the location of the shared_key file"
   default     = "~/.aws/credentials"
 }
+
+variable "cidr_block" {
+  description = "This variable states the CIDR block"
+  default     = "192.168.0.0/16"
+}
+
+variable "sn_cidr_block" {
+  description = "This variable states the subnet_a cidr_block"
+  default     = "192.168.1.0/24"
+}
